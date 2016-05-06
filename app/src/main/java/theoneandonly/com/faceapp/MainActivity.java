ead847ef169129160b5457b5293a14604bec64c3
benjamenngo <benjamenngo@gmail.com>
2016-05-05 22:56:34 -0400

    Added SplashScreen

--- app/src/main/java/theoneandonly/com/faceapp/MainActivity.java
+++ app/src/main/java/theoneandonly/com/faceapp/MainActivity.java
@@ -1,27 +1,48 @@
 package theoneandonly.com.faceapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import java.io.*;
-import android.app.*;
-import android.content.*;
-import android.net.*;
-import android.os.*;
-import android.view.*;
-import android.graphics.*;
-import android.widget.*;
-import android.provider.*;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
-import com.microsoft.projectoxford.face.*;
-import com.microsoft.projectoxford.face.contract.*;
+
+import com.microsoft.projectoxford.face.FaceServiceClient;
+import com.microsoft.projectoxford.face.FaceServiceRestClient;
+import com.microsoft.projectoxford.face.contract.Face;
+import com.microsoft.projectoxford.face.contract.FaceRectangle;
 
 
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.*;
+import java.net.URISyntaxException;
+
+import org.apache.http.client.methods.HttpPostHC4;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.*;
 
 public class MainActivity extends AppCompatActivity {
     private final int PICK_IMAGE = 1;
     private ProgressDialog detectionProgressDialog;
     private FaceServiceClient faceServiceClient =
             new FaceServiceRestClient("bdf3c987855948e0a9447d954e99734e");
+    CloseableHttpClient httpclient = HttpClients.createDefault();
+
+
 
 
     @Override
@@ -58,93 +79,97 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
-        private void detectAndFrame(final Bitmap imageBitmap)
-        {
-            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-            imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
-            ByteArrayInputStream inputStream =
-                    new ByteArrayInputStream(outputStream.toByteArray());
-            AsyncTask<InputStream, String, Face[]> detectTask =
-                    new AsyncTask<InputStream, String, Face[]>() {
-                        @Override
-                        protected Face[] doInBackground(InputStream... params) {
-                            try {
-                                publishProgress("Detecting...");
-                                Face[] result = faceServiceClient.detect(
-                                        params[0],
-                                        true,         // returnFaceId
-                                        false,        // returnFaceLandmarks
-                                        null           // returnFaceAttributes: a string like "age, gender"
-                                );
-                                if (result == null)
-                                {
-                                    publishProgress("Detection Finished. Nothing detected");
-                                    return null;
-                                }
-                                publishProgress(
-                                        String.format("Detection Finished. %d face(s) detected",
-                                                result.length));
-                                return result;
-                            } catch (Exception e) {
-                                publishProgress("Detection failed");
+
+    private void detectAndFrame(final Bitmap imageBitmap) {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+        ByteArrayInputStream inputStream =
+                new ByteArrayInputStream(outputStream.toByteArray());
+
+        AsyncTask<InputStream, String, Face[]> detectTask =
+                new AsyncTask<InputStream, String, Face[]>() {
+                    @Override
+                    protected Face[] doInBackground(InputStream... params) {
+                        try {
+                            publishProgress("Detecting...");
+                            Face[] result = faceServiceClient.detect(
+                                    params[0],
+                                    true,         // returnFaceId
+                                    false,        // returnFaceLandmarks
+                                    null           // returnFaceAttributes: a string like "age, gender"
+                            );
+                            if (result == null) {
+                                publishProgress("Detection Finished. Nothing detected");
                                 return null;
                             }
+                            publishProgress(
+                                    String.format("Detection Finished. %d face(s) detected",
+                                            result.length));
+                            return result;
+                        } catch (Exception e) {
+                            publishProgress("Detection failed");
+                            return null;
                         }
-                        @Override
-                        protected void onPreExecute() {
-                            detectionProgressDialog.show();
-                        }
-                        @Override
-                        protected void onProgressUpdate(String... progress) {
-                            detectionProgressDialog.setMessage(progress[0]);
-                        }
-                        @Override
-                        protected void onPostExecute(Face[] result) {
-                            detectionProgressDialog.dismiss();
-                            if (result == null) return;
-                            ImageView imageView = (ImageView)findViewById(R.id.imageView1);
-                            imageView.setImageBitmap(drawFaceRectanglesOnBitmap(imageBitmap, result));
-                            imageView.setOnClickListener(new View.OnClickListener() {
-                                @Override
-                                public void onClick(View v) {
-                                    Context context = getApplicationContext();
-                                    CharSequence text = "I've been toasted";
-                                    int duration = Toast.LENGTH_SHORT;
+                    }
 
-                                    Toast toast = Toast.makeText(context, text, duration);
-                                    toast.show();
-                                }
-                            });
-                                imageBitmap.recycle();
+                    @Override
+                    protected void onPreExecute() {
+                        detectionProgressDialog.show();
+                    }
 
-                        }
-                    };
-            detectTask.execute(inputStream);
-        }
-        private static Bitmap drawFaceRectanglesOnBitmap(Bitmap originalBitmap, Face[] faces) {
-            Bitmap bitmap = originalBitmap.copy(Bitmap.Config.ARGB_8888, true);
-            Canvas canvas = new Canvas(bitmap);
-            Paint paint = new Paint();
-            paint.setAntiAlias(true);
-            paint.setStyle(Paint.Style.STROKE);
-            paint.setColor(Color.RED);
+                    @Override
+                    protected void onProgressUpdate(String... progress) {
+                        detectionProgressDialog.setMessage(progress[0]);
+                    }
 
-            int stokeWidth = 2;
-            paint.setStrokeWidth(stokeWidth);
-            if (faces != null) {
+                    @Override
+                    protected void onPostExecute(Face[] result) {
+                        detectionProgressDialog.dismiss();
+                        if (result == null) return;
+                        ImageView imageView = (ImageView) findViewById(R.id.imageView1);
+                        imageView.setImageBitmap(drawFaceRectanglesOnBitmap(imageBitmap, result));
+                        imageView.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Context context = getApplicationContext();
+                                CharSequence text = "I've been toasted";
+                                int duration = Toast.LENGTH_SHORT;
 
-                for (Face face : faces) {
-                    FaceRectangle faceRectangle = face.faceRectangle;
-                    canvas.drawRect(
-                            faceRectangle.left,
-                            faceRectangle.top,
-                            faceRectangle.left + faceRectangle.width,
-                            faceRectangle.top + faceRectangle.height,
-                            paint);
-                    System.out.println(faceRectangle.left + faceRectangle.top);
-                }
+                                Toast toast = Toast.makeText(context, text, duration);
+                                toast.show();
+                            }
+                        });
+                        imageBitmap.recycle();
 
-            }
-            return bitmap;
-        }
+                    }
+                };
+        detectTask.execute(inputStream);
     }
+
+    private static Bitmap drawFaceRectanglesOnBitmap(Bitmap originalBitmap, Face[] faces) {
+        Bitmap bitmap = originalBitmap.copy(Bitmap.Config.ARGB_8888, true);
+        Canvas canvas = new Canvas(bitmap);
+        Paint paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setColor(Color.RED);
+
+        int stokeWidth = 2;
+        paint.setStrokeWidth(stokeWidth);
+        if (faces != null) {
+
+            for (Face face : faces) {
+                FaceRectangle faceRectangle = face.faceRectangle;
+                canvas.drawRect(
+                        faceRectangle.left,
+                        faceRectangle.top,
+                        faceRectangle.left + faceRectangle.width,
+                        faceRectangle.top + faceRectangle.height,
+                        paint);
+                System.out.println(faceRectangle.left + faceRectangle.top);
+            }
+
+        }
+        return bitmap;
+    }
+}
